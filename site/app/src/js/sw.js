65ef213c141fb140792e0030e8e7edaeb6cd665d
Matt Levy <ducksoupdev@gmail.com>
2020-09-25 16:17:44 +0100

    chore: update manifest

---
+++ site/app/src/js/sw.js
@@ -0,0 +1,32 @@
+/* global fetch self caches */
+var cacheName = 'ficus-site-cache'
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/scribbler-doc.css',
+  '/css/scribbler-global.css',
+  '/css/scribbler-landing.css',
+  '/img/logo.svg'
+]
+
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Install')
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log('[ServiceWorker] Caching app shell')
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
+      return response || fetch(event.request)
+    })
+  )
+})

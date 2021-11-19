894143cac387e82c463d964ea9b6c3b5d617efd8
Matt Levy <ducksoupdev@gmail.com>
2021-11-19 16:45:29 +0000

    feat: split component extensions

--- types/with-lazy-render.d.ts
+++
@@ -1,3 +0,0 @@
-import { CustomElementOptions } from '@ficusjs/core'
-
-export declare function withLazyRender<T> (options: CustomElementOptions<T>)
--- types/with-styles.d.ts
+++
@@ -1,9 +0,0 @@
-import { CustomElementOptions } from '@ficusjs/core'
-
-export type StyleOrURLString = string
-
-export type ComponentOptionsWithStyles<T> = CustomElementOptions<T> & {
-  styles(): StyleOrURLString[] | StyleOrURLString
-}
-
-export declare function withStyles<I, T> (options: ComponentOptionsWithStyles<T>)

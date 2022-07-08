57a5aa2f8c06a92fa190dc41cb73d7c5aaadbb87
Matt Levy <ducksoupdev@gmail.com>
2022-07-08 17:40:14 +0100

    chore: update cypress config

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  video: false,
   reporter: 'junit',
   reporterOptions: {
     mochaFile: 'cypress/reports/junit/ci-test-output-[hash].xml',
     jenkinsMode: true,
     rootSuiteTitle: 'FicusJS Integration Tests',
-    testsuitesTitle: 'Cypress Tests',
+    testsuitesTitle: 'Cypress Tests'
   },
   e2e: {}
 })

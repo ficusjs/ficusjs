db1dd6903daa55e344e5026b8e8197dcc6529e05
Matt Levy <ducksoupdev@gmail.com>
2022-07-25 14:17:29 +0100

    fix: missing create machine docs nav (#14)
    * fix: missing create machine docs nav
    * fix: base url

--- site/src/_data/nav.js
+++ site/src/_data/nav.js
@@ -47,8 +47,9 @@ module.exports = {
     { url: '/state-machines/create-xstate-service/', title: 'createXStateService function' },
     { url: '/state-machines/get-xstate-service/', title: 'getXStateService function' },
     { url: '/state-machines/with-xstate-service/', title: 'withXStateService function' },
-    { url: '/state-machines/assign/', title: 'assign function' },
-    { url: '/state-machines/interpret/', title: 'interpret function' }
+    { url: '/state-machines/create-machine/', title: 'createMachine function' },
+    { url: '/state-machines/interpret/', title: 'interpret function' },
+    { url: '/state-machines/assign/', title: 'assign function' }
   ],
   'Application state': [
     { url: '/app-state/', title: 'Introduction' },
